c5d2150e1552cf10012f6cef8963a458d7b7c3d0
enalto <enaltogondrige@gmail.com>
2024-10-09 08:56:30 -0300

    problema data2 version1

--- src/main/java/com/enalto/eda/data1/Main.java
+++ src/main/java/com/enalto/eda/data1/Main.java
@@ -14,21 +14,26 @@ public class Main {
 
 class Data {
     private final String data;
+    private final String[] splitData;
 
     public Data(String data) {
         this.data = data;
+        this.splitData = data.split("/");
     }
 
     public Integer getDia() {
-        return Integer.valueOf(this.data.split("/")[0]);
+        return Integer.valueOf(splitData[0]);
+//        return Integer.valueOf(this.data.split("/")[0]);
     }
 
     public Integer getMes() {
-        return Integer.valueOf(this.data.split("/")[1]);
+        return Integer.valueOf(splitData[1]);
+//        return Integer.valueOf(this.data.split("/")[1]);
     }
 
     public Integer getAno() {
-        return Integer.valueOf(this.data.split("/")[2]);
+        return Integer.valueOf(splitData[2]);
+//        return Integer.valueOf(this.data.split("/")[2]);
     }
 
 }
